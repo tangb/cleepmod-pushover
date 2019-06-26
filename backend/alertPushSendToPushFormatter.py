af290d4e00c99b4ea96ed7f3401654985a8e0a26
Tangb <tanguy.bonneau@gmail.com>
2019-06-26 19:59:46 +0200

    Rename factory to broker

--- backend/alertPushSendToPushFormatter.py
+++ backend/alertPushSendToPushFormatter.py
@@ -8,14 +8,14 @@ class AlertPushSendToPushFormatter(Formatter):
     """
     Push data to PushProfile
     """
-    def __init__(self, events_factory):
+    def __init__(self, events_broker):
         """
         Constuctor
 
         Args:
-            events_factory (EventsFactory): events factory instance
+            events_broker (EventsBroker): events broker instance
         """
-        Formatter.__init__(self, events_factory, u'alert.push.send', AlertPushProfile())
+        Formatter.__init__(self, events_broker, u'alert.push.send', AlertPushProfile())
 
     def _fill_profile(self, event_values, profile):
         """
